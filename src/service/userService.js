bbad79fb58583039d7fc9034e87961c9ac95085b
rushikesh-kusekar <kusekarrushikesh45@gmail.com>
2025-01-09 08:41:05 +0530

    find user by id, jwt token, find all users ,logic added in userService

--- src/service/userService.js
+++ src/service/userService.js
@@ -1,3 +1,4 @@
+const { getUserIdFromToken } = require("../config/jwtProvider.js");
 const User = require ("../models/user.model.js");
 const bcrypt = require("bcrypt");
 
@@ -53,7 +54,7 @@ module.exports = {
             const user = await User.findById(userId).populate("addresses");
 
             if(!user){
-                throw new Error("user not found");
+                throw new Error("user not found with id -",userId);
                 
             }
 
@@ -64,4 +65,31 @@ module.exports = {
             
         }
      },
+
+     async findUserProfileByIdJwt(jwt){
+
+        //userid
+
+        try {
+            const userId =getUserIdFromToken(jwt);
+            const user = await this.findUserById(userId);
+
+            return user;
+        } catch (error) {
+            throw new Error(error.message);
+            
+            
+        }
+     },
+
+     async findAllUsers(){
+
+        try {
+            const users = await User.find();
+            return users;
+        } catch (error) {
+            throw new Error(error.message);
+            
+        }
+     }
 };
